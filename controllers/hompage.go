63755eaccf59d25d6afe2ad2a829f852d89ca5a6
Gabriel Karczewski <nickelghost@outlook.com>
2019-11-23 20:36:24 +0100

    Implement post listing on the homepage

--- controllers/hompage.go
+++ controllers/hompage.go
@@ -4,8 +4,23 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/nickelghost/cms/common"
+	"github.com/nickelghost/cms/models"
 )
 
 func Homepage(c echo.Context) error {
-	return c.Render(http.StatusOK, "homepage.html", nil)
+	db := c.(*common.CustomContext).DB
+	var posts []models.Post
+	db.Select([]string{
+		"id",
+		"title",
+		"content_preview",
+		"created_at",
+		"updated_at",
+	}).Find(&posts)
+	return c.Render(
+		http.StatusOK,
+		"homepage.html",
+		map[string]interface{}{"posts": posts},
+	)
 }
