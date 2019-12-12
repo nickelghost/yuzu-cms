b669226ddad46634227896140db74c042cbfc8e5
Gabriel Karczewski <nickelghost@outlook.com>
2019-12-12 19:38:28 +0100

    Add tests for DB init

---
+++ database/database_test.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+func TestInit(t *testing.T) {
+	godotenv.Load("../.env")
+	_, err := Init(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSL"),
+	)
+	if err != nil {
+		t.Errorf("Connection to the DB failed:\n%s", err.Error())
+	}
+}
+
+func TestInitMock(t *testing.T) {
+	godotenv.Load("../.env")
+	_, err := Init(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		"wrongpassword",
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSL"),
+	)
+	if err == nil {
+		t.Errorf("Didn't return an error on wrong password")
+	}
+}
