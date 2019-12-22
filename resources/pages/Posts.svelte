a1eebf8a0fa4712465532a7ac5edb3190243cc8c
Gabriel Karczewski <nickelghost@outlook.com>
2019-12-22 23:43:28 +0100

    Properly list posts

--- resources/pages/Posts.svelte
+++ resources/pages/Posts.svelte
@@ -1,5 +1,6 @@
 <script>
 import { onMount } from 'svelte';
+import { push } from 'svelte-spa-router';
 
 let posts = [];
 
@@ -9,11 +10,40 @@ onMount(async () => {
 });
 </script>
 
-<div>
+<style>
+.posts-page {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+}
+.table td {
+  text-align: center;
+}
+.table tr {
+  height: 30px;
+}
+.table tr:hover {
+  background-color: lightgray;
+  cursor: pointer;
+}
+</style>
+
+<div class="posts-page">
   <h1>Posts</h1>
-  <ul>
+  <table class="table">
+    <tr>
+      <th>ID</th>
+      <th>Title</th>
+      <th>Created At</th>
+      <th>Updated at</th>
+    </tr>
     {#each posts as post}
-      <li>{post.title}</li>
+      <tr on:click={() => push(`/posts/${post.id}`)}>
+        <td>{post.id}</td>
+        <td>{post.title}</td>
+        <td>{post.created_at}</td>
+        <td>{post.updated_at}</td>
+      </tr>
     {/each}
-  </ul>
+  </table>
 </div>
