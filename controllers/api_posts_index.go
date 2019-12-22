674db82fe5c2ef5e36b2bca58f4632e2c4d3f988
Gabriel Karczewski <nickelghost@outlook.com>
2019-12-22 23:26:51 +0100

    Implement proper response struct for index

--- controllers/api_posts_index.go
+++ controllers/api_posts_index.go
@@ -2,12 +2,21 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/nickelghost/cms/database"
 	"github.com/nickelghost/cms/models"
 	"github.com/nickelghost/cms/other"
 )
+
+// APIPostsIndexResponseItem represents an array item for the response
+type APIPostsIndexResponseItem struct {
+	ID        uint      `json:"id"`
+	Title     string    `json:"title"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
 
 // APIPostsIndex fetches all posts that were added
 func APIPostsIndex(c echo.Context) error {
@@ -20,7 +29,7 @@ func APIPostsIndex(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	var posts []models.Post
+	var res []APIPostsIndexResponseItem
 	for rows.Next() {
 		post := models.Post{}
 		err := rows.Scan(
@@ -32,7 +41,13 @@ func APIPostsIndex(c echo.Context) error {
 		if err != nil {
 			return err
 		}
-		posts = append(posts, post)
+		resItem := APIPostsIndexResponseItem{
+			ID:        post.ID,
+			Title:     post.Title,
+			CreatedAt: post.CreatedAt,
+			UpdatedAt: post.UpdatedAt,
+		}
+		res = append(res, resItem)
 	}
-	return c.JSON(http.StatusOK, posts)
+	return c.JSON(http.StatusOK, res)
 }
