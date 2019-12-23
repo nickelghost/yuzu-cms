c85b3fca6015bdcfb7c54a8de4036ad866e10a2c
Gabriel Karczewski <nickelghost@outlook.com>
2019-12-23 23:31:49 +0100

    Redo post create to the new format

--- handlers/api_posts_create.go
+++ handlers/api_posts_create.go
@@ -3,21 +3,31 @@ package handlers
 import (
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo"
 	"github.com/nickelghost/cms/db"
-	"github.com/nickelghost/cms/models"
 )
 
-type requestAPIPostsCreate struct {
+// APIPostsCreateRequest represents a request for creating a post
+type APIPostsCreateRequest struct {
 	Title   string `json:"title" validate:"required"`
 	Content string `json:"content" validate:"min=10"`
 }
 
+// APIPostsCreateResponse represents a response of a created post
+type APIPostsCreateResponse struct {
+	ID        uint      `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 // APIPostsCreate adds a new post for the website
 func APIPostsCreate(c echo.Context) error {
-	req := new(requestAPIPostsCreate)
+	req := new(APIPostsCreateRequest)
 	err := c.Bind(req)
 	if err != nil {
 		return err
@@ -26,24 +36,24 @@ func APIPostsCreate(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	post := models.Post{}
 	sql, err := ioutil.ReadFile("queries/api_posts_create.sql")
 	if err != nil {
 		return err
 	}
+	res := new(APIPostsCreateResponse)
 	err = db.Conn.QueryRow(
 		string(sql),
 		req.Title,
 		req.Content,
 	).Scan(
-		&post.ID,
-		&post.Title,
-		&post.Content,
-		&post.CreatedAt,
-		&post.UpdatedAt,
+		&res.ID,
+		&res.Title,
+		&res.Content,
+		&res.CreatedAt,
+		&res.UpdatedAt,
 	)
 	if err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK, post)
+	return c.JSON(http.StatusOK, res)
 }
