37c6576195a4e129f76243e8dd4ee9f23679dcbd
Gabriel Karczewski <nickelghost@outlook.com>
2019-12-27 00:42:10 +0100

    Add color options to the buttons

--- resources/components/Button.svelte
+++ resources/components/Button.svelte
@@ -1,11 +1,10 @@
 <script>
 export let label;
+export let color;
 import { createEventDispatcher } from 'svelte';
 
 const dispatch = createEventDispatcher();
-function onClick() {
-  dispatch('click')
-}
+function onClick() { dispatch('click') }
 </script>
 
 <style>
@@ -17,6 +16,30 @@ function onClick() {
   cursor: pointer;
   padding: 12px 24px;
 }
+.button-blue {
+  color: white;
+  background-color: cornflowerblue;
+}
+.button-red {
+  color: white;
+  background-color: crimson;
+}
+.button-green {
+  color: white;
+  background-color: rgb(0, 209, 28);
+}
+.button-yellow {
+  background-color: #ffd000;
+}
 </style>
 
-<button class="button" on:click={onClick}>{label}</button>
+<button
+  class="button"
+  class:button-blue={color === 'blue'}
+  class:button-red={color === 'red'}
+  class:button-green={color === 'green'}
+  class:button-yellow={color === 'yellow'}
+  on:click={onClick}
+>
+  {label}
+</button>
