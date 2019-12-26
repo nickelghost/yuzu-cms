d3f5d454956febe8723abc77cf3d906ce6876518
Gabriel Karczewski <nickelghost@outlook.com>
2019-12-26 18:35:05 +0100

    Implement title change modal

--- resources/pages/Post.svelte
+++ resources/pages/Post.svelte
@@ -6,9 +6,12 @@ import TopBar from '../components/TopBar.svelte';
 import Button from '../components/Button.svelte';
 import Content from '../components/Content.svelte';
 import Editor from '../components/Editor.svelte';
+import Modal from '../components/Modal.svelte';
 
 let title = "";
 let content = "";
+let isTitleModalOpen = false;
+let newTitle = "";
 
 onMount(async () => {
   const res = await fetch(`/api/v1/posts/${params.id}`);
@@ -16,6 +19,14 @@ onMount(async () => {
   title = post.title;
   content = post.content;
 });
+
+function openTitleModal() { isTitleModalOpen = true; }
+function closeTitleModal() { isTitleModalOpen = false; }
+function updateTitle() {
+  title = newTitle;
+  isTitleModalOpen = false;
+  newTitle = "";
+}
 
 async function savePost() {
   const req = { title, content };
@@ -35,7 +46,43 @@ async function savePost() {
 }
 </script>
 
+<style>
+.title-modal {
+  display: flex;
+  flex-direction: column;
+  width: 480px;
+}
+.title-modal-label {
+  font-size: 1.125rem;
+  margin-bottom: 8px;
+}
+.title-modal-input {
+  margin-bottom: 16px;
+}
+.title-modal-buttons {
+  display: flex;
+}
+.spacer {
+  flex-grow: 1;
+}
+</style>
+
+<Modal bind:isOpen={isTitleModalOpen}>
+  <div class="title-modal">
+    <label class="title-modal-label">
+      Enter the new title:
+    </label>
+    <input class="title-modal-input" bind:value={newTitle}>
+    <div class="title-modal-buttons">
+      <Button label="Cancel" on:click={closeTitleModal}></Button>
+      <div class="spacer"></div>
+      <Button label="Change" on:click={updateTitle}></Button>
+    </div>
+  </div>
+</Modal>
+
 <TopBar title={title}>
+  <Button label="Change title" on:click={openTitleModal}></Button>
   <Button label="Save" on:click={savePost}></Button>
 </TopBar>
 <Content>
