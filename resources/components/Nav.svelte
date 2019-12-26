31dfff833f6c959b11ef69b2ab24682425c028c2
Gabriel Karczewski <nickelghost@outlook.com>
2019-12-26 13:52:42 +0100

    Style the nav

--- resources/components/Nav.svelte
+++ resources/components/Nav.svelte
@@ -1,12 +1,30 @@
 <style>
 .nav {
-  background-color: #3a243b;
+  background: rgb(78,0,121);
+  background: linear-gradient(90deg, rgba(78,0,121,1) 0%, rgba(156,0,200,1) 100%);
   box-sizing: border-box;
   color: white;
   display: flex;
   flex-direction: column;
-  padding: 0 12px;
   width: 260px;
+}
+.heading {
+  text-align: center;
+}
+.nav-button {
+  font-size: 1.125rem;
+  display: inline-block;
+  padding: 12px 24px;
+}
+.nav-button-underline {
+  background-color: white;
+  height: 2px;
+  margin-top: 4px;
+  visibility: hidden;
+  width: 100%;
+}
+.nav-link:hover .nav-button-underline {
+  visibility: visible;
 }
 .spacer {
   flex-grow: 1;
@@ -17,11 +35,31 @@ a {
 }
 </style>
 
-<div class="nav">
-  <h1>Admin</h1>
-  <p><a href="/admin/#/">Home</a></p>
-  <p><a href="/admin/#/posts">Posts</a></p>
-  <p><a href="/admin/#/pages">Pages</a></p>
+<aside class="nav">
+  <h1 class="heading">Yuzu</h1>
+  <a class="nav-link" href="/admin/#/">
+    <div class="nav-button">
+      Home
+      <div class="nav-button-underline" />
+    </div>
+  </a>
+  <a class="nav-link" href="/admin/#/posts">
+    <div class="nav-button">
+      Posts
+      <div class="nav-button-underline" />
+    </div>
+  </a>
+  <a class="nav-link" href="/admin/#/pages">
+    <div class="nav-button">
+      Pages
+      <div class="nav-button-underline" />
+    </div>
+  </a>
   <div class="spacer"></div>
-  <p><a href="/">Website</a></p>
-</div>
+  <a class="nav-link" href="/">
+    <div class="nav-button">
+      Website
+      <div class="nav-button-underline" />
+    </div>
+  </a>
+</aside>
