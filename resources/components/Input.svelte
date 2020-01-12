01795286bed01f757e619bb899bd186a392e4371
Gabriel Karczewski <nickelghost@outlook.com>
2020-01-12 02:28:52 +0100

    Create the custom input component

---
+++ resources/components/Input.svelte
@@ -0,0 +1,13 @@
+<script>
+export let value;
+</script>
+
+<style>
+.input {
+  border: 2px solid lightgrey;
+  border-radius: 8px;
+  padding: 8px;
+}
+</style>
+
+<input class="input" {...$$props} bind:value={value}>
