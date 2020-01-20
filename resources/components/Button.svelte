a905ba198814b1876873266ab07b7ffac27427da
Gabriel Karczewski <nickelghost@outlook.com>
2020-01-20 23:38:48 +0100

    Redesign the button

--- resources/components/Button.svelte
+++ resources/components/Button.svelte
@@ -1,8 +1,6 @@
 <script>
-export let label;
-export let color;
 import { createEventDispatcher } from 'svelte';
-
+export let label;
 const dispatch = createEventDispatcher();
 function onClick() { dispatch('click') }
 </script>
@@ -10,36 +8,12 @@ function onClick() { dispatch('click') }
 <style>
 .button {
   background-color: white;
-  border: 1px solid lightgrey;
-  border-radius: 4px;
-  color: #222;
+  border: 1px solid #ececec;
   cursor: pointer;
   padding: 12px 24px;
 }
-.button-blue {
-  color: white;
-  background-color: cornflowerblue;
-}
-.button-red {
-  color: white;
-  background-color: crimson;
-}
-.button-green {
-  color: white;
-  background-color: rgb(0, 209, 28);
-}
-.button-yellow {
-  background-color: #ffd000;
-}
 </style>
 
-<button
-  class="button"
-  class:button-blue={color === 'blue'}
-  class:button-red={color === 'red'}
-  class:button-green={color === 'green'}
-  class:button-yellow={color === 'yellow'}
-  on:click={onClick}
->
+<button class="button" on:click={onClick}>
   {label}
 </button>
