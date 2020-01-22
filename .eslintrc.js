a6a1c08cde97060ef5c822e56fdd48d93d6a5fb0
Gabriel Karczewski <nickelghost@outlook.com>
2020-01-22 06:50:26 +0100

    Add Svelte integration for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,15 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
   rules: {
   },
 };
