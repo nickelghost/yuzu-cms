4e74a114c400bc19eb606a218fa385710e418d9c
Gabriel Karczewski <nickelghost@outlook.com>
2020-01-22 07:21:54 +0100

    Fix rules that break with Svelte

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,9 @@ module.exports = {
     }
   ],
   rules: {
+    'import/first': 0,
+    'import/no-mutable-exports': 0,
+    'import/prefer-default-export': 0,
+    'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 2, maxEOF: 0 }],
   },
 };
