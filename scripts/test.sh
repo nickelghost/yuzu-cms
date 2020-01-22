623adfaeac444e0ca0654681b71c821e8463ddcf
Gabriel Karczewski <nickelghost@outlook.com>
2020-01-22 18:02:51 +0100

    Update db package path

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
-go test github.com/nickelghost/cms/database -v -count 1 || exit 1
+go test github.com/nickelghost/cms/db -v -count 1 || exit 1
 go test github.com/nickelghost/cms/models -v -count 1 || exit 1
