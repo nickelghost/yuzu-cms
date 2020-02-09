9a486f07a1ebc280ffcfb311d10f95f117f72777
Gabriel Karczewski <nickelghost@outlook.com>
2020-02-09 16:03:04 +0100

    Format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,46 +5,43 @@ const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
 module.exports = {
-	entry: {
-		bundle: ['./resources/index.js']
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
-		},
-		extensions: ['.mjs', '.js', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: __dirname + '/public',
-		filename: 'index.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						emitCss: true,
-						hotReload: true
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader'
-				]
-			}
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: 'index.css'
-		})
-	],
-	devtool: prod ? false: 'source-map'
+  entry: {
+    bundle: ['./resources/index.js'],
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte'),
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main'],
+  },
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            emitCss: true,
+            hotReload: true,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+    }),
+  ],
+  devtool: prod ? false : 'source-map',
 };
