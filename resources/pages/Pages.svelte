a146b5f0f23ad43128dc66d36c82d00c8bc5eedb
Gabriel Karczewski <nickelghost@outlook.com>
2020-04-23 00:26:18 +0200

    Redesign the pages page Closes #48

--- resources/pages/Pages.svelte
+++ resources/pages/Pages.svelte
@@ -2,17 +2,21 @@
   import { onMount } from 'svelte';
 
   import TopBar from '../components/TopBar.svelte';
+  import Modal from '../components/Modal.svelte';
+  import PageForm from './Pages/PageForm.svelte';
 
+  import notify from '../helpers/notify';
   import { jwt } from '../stores';
 
   let pages = [];
   let posts = [];
 
-  const form = {
-    slug: '',
-    in_navigation: true,
-    post_id: 0,
-  };
+  let isNewModalOpen = false;
+  let isEditModalOpen = false;
+
+  // eslint-disable-next-line prefer-const
+  let newPageForm = {};
+  let editPageForm = {};
 
   async function getPages() {
     const res = await fetch('/api/v1/pages', {
@@ -37,8 +41,9 @@
     getPosts();
   });
 
-  async function addPage() {
-    // eslint-disable-next-line
+  async function addPage(e) {
+    e.preventDefault();
+    // eslint-disable-next-line operator-linebreak
     const highestIndex =
       pages.length === 0
         ? 0
@@ -50,24 +55,52 @@
         Authorization: `Bearer ${$jwt}`,
       },
       body: JSON.stringify({
-        ...form,
+        ...newPageForm,
         index: highestIndex + 1,
       }),
     });
     if (res.ok) {
+      notify('Page added', 'green');
       getPages();
+      isNewModalOpen = false;
+    } else {
+      notify('Could not add the page', 'red');
+    }
+  }
+
+  async function updatePage(e) {
+    e.preventDefault();
+    const req = {
+      ...editPageForm,
+      position_change: 0,
+    };
+    const res = await fetch(`/api/v1/pages/${req.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${$jwt}`,
+      },
+      body: JSON.stringify(editPageForm),
+    });
+    if (res.ok) {
+      getPages();
+      notify('Page updated', 'green');
+      isEditModalOpen = false;
+    } else {
+      notify('Could not update the page', 'red');
     }
   }
 
   async function deletePage(id) {
     const res = await fetch(`/api/v1/pages/${id}`, {
       method: 'DELETE',
-      headers: {
-        Authorization: `Bearer ${$jwt}`,
-      },
+      headers: { Authorization: `Bearer ${$jwt}` },
     });
     if (res.ok) {
+      notify('Page deleted', 'green');
       getPages();
+    } else {
+      notify('Could not delete the page', 'red');
     }
   }
 
@@ -85,7 +118,10 @@
       body: JSON.stringify(req),
     });
     if (res.ok) {
+      notify('Page position updated', 'green');
       getPages();
+    } else {
+      notify('Could not update page position', 'red');
     }
   }
 </script>
@@ -94,49 +130,120 @@
   th {
     text-align: left;
   }
+
+  .table {
+    font-size: 1.125rem;
+    width: 100%;
+  }
+
+  .table th {
+    font-size: 0.875rem;
+    font-weight: 500;
+    text-transform: uppercase;
+  }
+
+  .slug {
+    font-family: 'Fira Code', monospace;
+  }
 </style>
 
-<TopBar title="Pages"></TopBar>
+<TopBar title="Pages">
+  <button
+    class="button button-primary"
+    on:click="{() => { isNewModalOpen = true; }}"
+  >
+    New
+  </button>
+</TopBar>
+
+<Modal bind:isOpen="{isNewModalOpen}">
+  <form class="form" on:submit="{addPage}">
+    <PageForm
+      posts="{posts}"
+      bind:post_id="{newPageForm.post_id}"
+      bind:slug="{newPageForm.slug}"
+      bind:in_navigation="{newPageForm.in_navigation}"
+    ></PageForm>
+    <div class="field field-horizontal">
+      <button
+        class="button"
+        type="button"
+        on:click="{() => { isNewModalOpen = false; }}"
+      >
+        Cancel
+      </button>
+      <div class="flex-spacer"></div>
+      <button class="button button-primary" type="submit">
+        Add
+      </button>
+    </div>
+  </form>
+</Modal>
+
+<Modal bind:isOpen="{isEditModalOpen}">
+  <form class="form" on:submit="{updatePage}">
+    <PageForm
+      posts="{posts}"
+      bind:post_id="{editPageForm.post_id}"
+      bind:slug="{editPageForm.slug}"
+      bind:in_navigation="{editPageForm.in_navigation}"
+    ></PageForm>
+    <div class="field field-horizontal">
+      <button
+        class="button"
+        type="button"
+        on:click="{() => { isEditModalOpen = false; }}"
+      >
+        Cancel
+      </button>
+      <div class="flex-spacer"></div>
+      <button class="button button-primary" type="submit">
+        Change
+      </button>
+    </div>
+  </form>
+</Modal>
 
 <div class="content">
-  <table>
+  <table class="table">
     <tr>
       <th>Position</th>
+      <th>Post</th>
       <th>Slug</th>
       <th>In navigation</th>
-      <th>Post</th>
       <th></th>
-    </tr>
-    <tr>
-      <td></td>
-      <td><input type="text" bind:value="{form.slug}" /></td>
-      <td><input type="checkbox" bind:checked="{form.in_navigation}" /></td>
-      <td>
-        <select bind:value="{form.post_id}">
-          <option value="{0}" disabled></option>
-          {#each posts as post}
-          <option value="{post.id}">{post.title}</option>
-          {/each}
-        </select>
-      </td>
-      <td>
-        <button class="button button-primary" on:click="{addPage}">
-          Add
-        </button>
-      </td>
     </tr>
     {#each pages as page}
     <tr>
       <td>
+        <button
+          class="button button-small"
+          on:click="{() => changePageIndex(page, 1)}"
+        >
+          +
+        </button>
         {page.index}
-        <button on:click={() => changePageIndex(page, 1)}>+</button>
-        <button on:click={() => changePageIndex(page, -1)}>-</button>
+        <button
+          class="button button-small"
+          on:click="{() => changePageIndex(page, -1)}"
+        >
+          -
+        </button>
       </td>
-      <td>{page.slug}</td>
-      <td><input type="checkbox" disabled checked="{page.in_navigation}" /></td>
       <td>{page.post.title}</td>
+      <td class="slug">{page.slug}</td>
+      <td>{page.in_navigation ? 'Yes' : 'No'}</td>
       <td>
-        <button class="button" on:click="{() => deletePage(page.id)}">
+        <button
+          class="button"
+          on:click="{() => { editPageForm = { ...page }; isEditModalOpen = true; }}"
+        >
+          Edit
+        </button>
+        <button
+          class="button button-danger"
+          on:click="{() => deletePage(page.id)}"
+        >
           Delete
         </button>
       </td>
---
+++ resources/pages/Pages/PageForm.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let posts;
+  export let post_id;
+  export let slug;
+  export let in_navigation;
+</script>
+
+<label class="field">
+  <span class="label">Post</span>
+  <select class="input" bind:value="{post_id}">
+    <option value="{0}" disabled></option>
+    {#each posts as post}
+    <option value="{post.id}">{post.title}</option>
+    {/each}
+  </select>
+</label>
+<label class="field">
+  <span class="label">Slug</span>
+  <input class="input" bind:value="{slug}" />
+</label>
+<label class="field">
+  <span class="label">In navigation</span>
+  <input class="input" type="checkbox" bind:checked="{in_navigation}" />
+</label>
