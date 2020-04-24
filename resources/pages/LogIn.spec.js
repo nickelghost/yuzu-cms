6edb1e62966a4476b095f55acc8f1cd294147eae
Gabriel Karczewski <nickelghost@outlook.com>
2020-04-25 01:04:55 +0200

    Finish tests for the LogIn page

--- resources/pages/LogIn.spec.js
+++ resources/pages/LogIn.spec.js
@@ -1,6 +1,8 @@
-import { render } from '@testing-library/svelte';
+import { render, fireEvent } from '@testing-library/svelte';
+import { get } from 'svelte/store';
 
 import LogIn from './LogIn.svelte';
+import { jwt, notifications } from '../stores';
 
 describe('Log in page', () => {
   it('has a Yuzu heading', () => {
@@ -8,13 +10,46 @@ describe('Log in page', () => {
     const heading = getByText('Yuzu');
     expect(heading).toBeInstanceOf(HTMLHeadingElement);
   });
-  it('has user and password inputs', () => {
-    const { getByLabelText } = render(LogIn);
-    expect(getByLabelText('User')).toBeInstanceOf(HTMLInputElement);
-    expect(getByLabelText('Password')).toBeInstanceOf(HTMLInputElement);
+  it('sends the log in form', async () => {
+    // Mock the fetch response
+    global.fetch = jest.fn().mockImplementation(async () => ({
+      ok: true,
+      json: async () => ({ token: 'test-token' }),
+    }));
+    // Render and interact with DOM
+    const { getByLabelText, getByText } = render(LogIn);
+    const userInput = getByLabelText('User');
+    const passwordInput = getByLabelText('Password');
+    await Promise.all([
+      fireEvent.input(userInput, { target: { value: 'my-user' } }),
+      fireEvent.input(passwordInput, { target: { value: 'my-password' } }),
+    ]);
+    const button = getByText('Log in');
+    await fireEvent.click(button);
+    // Assert fetch call
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('/api/v1/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        user: 'my-user',
+        password: 'my-password',
+      }),
+    });
+    // Check if jwt in the state was updated
+    expect(get(jwt)).toBe('test-token');
+    // Check if the user was notified
+    expect(
+      get(notifications).find((n) => n.message === 'Could not log in'),
+    ).not.toBeNull();
   });
-  it('has a log in button', () => {
+  it('notifies on login fail', async () => {
+    global.fetch = jest.fn().mockImplementation(async () => ({ ok: false }));
     const { getByText } = render(LogIn);
-    expect(getByText('Log in')).toBeInstanceOf(HTMLButtonElement);
+    const button = getByText('Log in');
+    await fireEvent.click(button);
+    expect(
+      get(notifications).find((n) => n.message === 'Could not log in'),
+    ).not.toBeNull();
   });
 });
